0f134c51b67d989a7c103b787860adbc2d68beb6
Daniel Veloso <danielveloso09@gmail.com>
2020-06-08 00:19:48 +0100

    fix typo on fzf installer

--- dependencies/fzf.sh
+++ dependencies/fzf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Installing fzf"
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/instal
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
